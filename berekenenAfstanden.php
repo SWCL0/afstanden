4eb3093b3cc786241ad50c0dfb0edb9588348a91
SWCL0 <stan.w.c@icloud.com>
2024-02-12 08:54:05 +0100

    update afstanden.html
    bestand veranderd naar html, dit zodat ik form kan post method naar php bestand. geeft duidelijkheid. Verder de name/id's aangepast

---
+++ berekenenAfstanden.php
@@ -0,0 +1,11 @@
+<?php
+
+$waarde = $_POST["waarde"];
+$currentGrootheid = $_POST["currentGrootheden"];
+$gewensteGrootheid = $_POST["gewensteGrootheden"];
+
+echo $waarde;
+echo $currentGrootheid;
+echo $gewensteGrootheid;
+
+?>
