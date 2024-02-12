1710baa696dce9aa69a8daf311968d452d2f52fb
SWCL0 <stan.w.c@icloud.com>
2024-02-12 08:19:53 +0100

    create afstanden.php
    aanmaak bestand
    opzet html
    begin form

---
+++ afstanden.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Afstanden Omrekenen</title>
+</head>
+<body>
+    <h1>Afstanden omrekenen</h1>
+    <div>
+        <form>
+            <label for="waarde">Welke waarde:</label>
+            <input type="number">
+        </form>
+    </div>
+</body>
+</html>
